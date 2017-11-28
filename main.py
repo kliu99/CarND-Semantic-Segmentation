7270c8f80ee9a0590b9de24cbc89dc3401ceb7d3
Kai Liu <kliu99@gatech.edu>
2017-11-28 10:32:40 -0500

    basic implementation done

--- main.py
+++ main.py
@@ -60,10 +60,12 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     # 1x1 Convolutional layer: maintain spatial information, adjust depth.
     # 1x1 conv (?x5x18x4096) -> (?x5x18x2)
     vgg_layer7_conv = tf.layers.conv2d(vgg_layer7_out, num_classes, kernel_size=1, strides=1, padding='same',
+                                       kernel_initializer=tf.random_normal_initializer(stddev=0.01),
                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     # 1x1 conv (?x10x36x512) -> (?x10x36x2)
     vgg_layer4_conv = tf.layers.conv2d(vgg_layer4_out, num_classes, kernel_size=1, strides=1, padding='same',
+                                       kernel_initializer=tf.random_normal_initializer(stddev=0.01),
                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     # 1x1 conv (?x20x72x256) -> (?x20x72x2)
@@ -76,18 +78,21 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
 
     # 2x Up sampling (?x5x18x2) -> (?x10x36x2)
     output = tf.layers.conv2d_transpose(output, num_classes, kernel_size=4, strides=2, padding='same',
-                                       kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
+                                        kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                        kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     # Add skip layer
     output = tf.add(output, vgg_layer4_conv)
 
     # 2x Up sampling (?x10x36x2) -> (?x20x72x2)
     output = tf.layers.conv2d_transpose(output, num_classes, kernel_size=4, strides=2, padding='same',
+                                        kernel_initializer=tf.random_normal_initializer(stddev=0.01),
                                         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
     # Add skip layer
     output = tf.add(output, vgg_layer3_conv)
 
     # 8x Up sampling (?x20x72x2) -> (?x160x576x2)
     output = tf.layers.conv2d_transpose(output, num_classes, kernel_size=16, strides=8, padding='same',
+                                        kernel_initializer=tf.random_normal_initializer(stddev=0.01),
                                         kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3))
 
     # Return shape: batch_size x image_width x image_height x num_classes
@@ -104,11 +109,13 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
+    # DONE: Implement function
 
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+    train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cross_entropy_loss)
 
-
-    return None, None, None
+    return logits, train_op, cross_entropy_loss
 tests.test_optimize(optimize)
 
 
@@ -127,8 +134,22 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
-    pass
+    # DONE: Implement function
+
+    keep_prob_value = 0.5
+    learning_rate_value = 1e-2
+
+    for epoch in range(epochs):
+        for image, label in get_batches_fn(batch_size):
+            _, loss = sess.run([train_op, cross_entropy_loss], feed_dict={
+                input_image: image,
+                correct_label: label,
+                keep_prob: keep_prob_value,
+                learning_rate: learning_rate_value
+            })
+
+            print('Epoch {:>2} Loss: {:>10.4f}'.format(epoch + 1, loss))
+
 tests.test_train_nn(train_nn)
 
 
@@ -146,7 +167,18 @@ def run():
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
+    # Parameters
+    learning_rate = tf.placeholder("float", name="learning_rate")
+    correct_label = tf.placeholder("float", shape=[None, None, None, num_classes], name="correct_label")
+    input_image = tf.placeholder("float", shape=[None, image_shape[0], image_shape[1], 3], name="input_image")
+    epochs = 10
+    batch_size = 1
+
     with tf.Session() as sess:
+
+        # Initializing the variables
+        sess.run(tf.global_variables_initializer())
+
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
@@ -155,12 +187,16 @@ def run():
         # OPTIONAL: Augment Images for better results
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
-        # TODO: Build NN using load_vgg, layers, and optimize function
+        # DONE: Build NN using load_vgg, layers, and optimize function
+        image_input, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
+        layer_output = layers(layer3_out, layer4_out, layer7_out, num_classes)
+        logits, train_op, cross_entropy_loss = optimize(layer_output, correct_label, learning_rate, num_classes)
 
-        # TODO: Train NN using the train_nn function
-
-        # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        # DONE: Train NN using the train_nn function
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+                 correct_label, keep_prob, learning_rate)
+        # DONE: Save inference data using helper.save_inference_samples
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
 
